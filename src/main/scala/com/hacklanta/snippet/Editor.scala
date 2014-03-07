22d34b83057f9b0726f433231ab127a5f9bf2a1e
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2014-03-07 01:03:27 -0500

    Add running of sbt and streaming output/pushing input to/from browser.

--- src/main/scala/com/hacklanta/snippet/Editor.scala
+++ src/main/scala/com/hacklanta/snippet/Editor.scala
@@ -1,9 +1,16 @@
 package com.hacklanta
 package snippet
 
-import io._
+import java.io._
+import java.util.concurrent.{BlockingQueue,LinkedBlockingQueue}
 
+import scala.io._
+import scala.sys.process._
+
+import net.liftweb.http._
 import net.liftweb.util.Helpers._
+
+import com.hacklanta.rest.projectBaseDirectory
 
 object Editor {
   def render = {
@@ -11,4 +18,65 @@ object Editor {
 
     ".content *" #> file
   }
+
+  def sbtRunner = {
+    @volatile var stdinStream: Option[PrintStream] = None
+
+    def bidirectionalProcessIo(setStdinStream: (PrintStream)=>Unit, outputLineQueue: BlockingQueue[Option[String]]) = {
+      new ProcessIO(
+        { runningStdinStream =>
+          setStdinStream(new PrintStream(runningStdinStream))
+        },
+        { stdoutStream =>
+          val reader = new BufferedReader(new InputStreamReader(stdoutStream))
+
+          Stream
+            .continually(reader.readLine)
+            .takeWhile(_ != null)
+            .foreach { line =>
+              outputLineQueue.put(Some(line))
+            }
+          outputLineQueue.add(None)
+        },
+        _ => ()
+      )
+    }
+
+    val runnerJs =
+      for {
+        session <- S.session
+        directory <- projectBaseDirectory.is
+        outputQueue = new LinkedBlockingQueue[Option[String]]()
+        sbtProcess = Process("sbt" :: Nil, directory.toFile)
+        roundtripJs =
+          session.buildRoundtrip(List[RoundTripInfo](
+            "startSbt" -> { _: String =>
+              sbtProcess.run(
+                bidirectionalProcessIo(
+                  printStream => stdinStream = Some(printStream),
+                  outputQueue
+                )
+              )
+
+              Stream
+                .continually(outputQueue.take)
+                .takeWhile(_ != None)
+            },
+            "runSbtCommand" -> { command: String =>
+              stdinStream.map { stream =>
+                stream.println(command)
+                stream.flush
+              }
+            },
+            "stopSbt" -> { _: String =>
+              stdinStream.map(_.close)
+            }
+          ))
+      } yield {
+        s"var sbtRunner = $roundtripJs"
+      }
+
+    "script *" #> runnerJs
+  }
+
 }
