94163beabd7d64f672fdbc655d3aec0ac1cb5438
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2014-03-08 20:14:54 -0500

    Strip the file endpoint directory when it appears in sbt output.

--- src/main/scala/com/hacklanta/snippet/Editor.scala
+++ src/main/scala/com/hacklanta/snippet/Editor.scala
@@ -6,6 +6,7 @@ import net.liftweb.http._
 import net.liftweb.util.Helpers._
 
 import lib.SbtInteractor
+import com.hacklanta.rest.fileEndpointDirectory
 
 object interactor extends SessionVar[Box[SbtInteractor]](SbtInteractor())
 object Editor {
@@ -16,7 +17,17 @@ object Editor {
         interactor <- interactor.is
         roundtripJs =
           session.buildRoundtrip(List[RoundTripInfo](
-            "connect" -> { _: String => interactor.output },
+            "connect" -> { _: String =>
+              for {
+                endpointDirectory <- fileEndpointDirectory.is.toStream
+                stringToStrip = endpointDirectory + "/"
+                possibleLine <- interactor.output
+                line <- possibleLine
+              } yield {
+                line
+                  .replace(stringToStrip, "")
+              }
+            },
             "runCommand" -> interactor.runCommand _,
             "stop" -> { _: String => interactor.stop() }
           ))
