bed245cc15e01a3ba8c89a9b9ff108b8398d7cfd
Antonio Salazar Cardozo <savedfastcool@gmail.com>
2014-03-20 23:59:13 -0400

    Add LiftEvent class.
    Represents a basic event to send down to the client. LiftEvent
    takes an event and data to send down, while BasicLiftEvent
    extracts the data by serializing the event object itself via
    lift-json (well-suited for case classes). If BasicLiftEvent’s event
    name is left off, it is inferred from the class name.

---
+++ src/main/scala/com/hacklanta/lib/LiftEvent.scala
@@ -0,0 +1,34 @@
+package com.hacklanta
+package snippet
+
+import net.liftweb._
+  import http._
+    import js._
+      import JE._
+  import json._
+    import Extraction._
+
+class LiftEvent(event:String, data:JObject = JObject(List())) extends JsCmd {
+  override val toJsCmd = { 
+    if (data.obj.length == 0)
+      Call("liftAjax.event", event).cmd.toJsCmd
+    else
+      Call("liftAjax.event", event, data).cmd.toJsCmd
+  }   
+}
+class BasicLiftEvent(event: String = null) extends JsCmd {
+  implicit val formats = DefaultFormats
+
+  private lazy val eventName = Option(event) getOrElse BasicLiftEvent.eventNameFromObject(this)
+
+  override val toJsCmd = {
+    Call("liftAjax.event", eventName, decompose(this)).cmd.toJsCmd
+  }
+}
+object BasicLiftEvent {
+  def eventNameFromObject(eventObject: BasicLiftEvent) = {
+    eventObject.getClass.getName
+      .replaceAll("([A-Z][a-z\\d]+)(?=([A-Z][a-z\\d]+))", "$1-")
+      .toLowerCase
+  }
+}
